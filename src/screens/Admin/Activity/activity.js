cac6581539cf976fc6debd9a9bcd46bf5dbb870f
thominh <thominh25111997@gmail.com>
2022-06-20 20:28:26 +0700

    update activity

--- src/screens/Admin/Activity/activity.js
+++ src/screens/Admin/Activity/activity.js
@@ -1,12 +1,33 @@
 /* eslint-disable prettier/prettier */
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
+import { eventMaterialAPI } from "../../../api/event-material.api";
 import Header from "../../../components/Header";
-export default function Activity (){
+import ItemActivity from "./item-activity";
+export default function Activity({navigation}) {
+    const title = "Hoạt động";
+    const [events, setEvents] = useState([]);
+    const fetchEvents = async () => {
+        try {
+            const { data } = await eventMaterialAPI.get();
+            setEvents(data)
+        } catch (error) {
+
+        }
+    }
+
+    useEffect(() => {
+        fetchEvents();
+    }, [])
     return (
         <SafeAreaView>
-            <Header>Hoạt động</Header>
-            <View></View>
+            <Header>{title}</Header>
+            <View style={{
+                padding: 10
+            }}>
+                {events.length > 0 ? <FlatList data={events} renderItem={({ item }) => <ItemActivity item={item} callback={() => fetchMaterials()} navigation={navigation} />} keyExtractor={(item, index) => index.toString()} /> :
+                    <Text>Hiện tại không có hoạt động</Text>}
+            </View>
         </SafeAreaView>
     )
 }
